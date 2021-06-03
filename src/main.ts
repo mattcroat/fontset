c6d754bb41d345cfa3949e4dda957e7a37e350cc
mattcroat <mattcroat.dev@gmail.com>
2021-06-03 23:36:29 +0200

    feat: Add getFontURL method, state refactor, types

--- src/main.ts
+++ src/main.ts
@@ -3,30 +3,69 @@ import './styles/tailwind.css'
 import variableFonts from '@root/data/variableFonts.json'
 import characterTable from '@root/data/characterTable.json'
 
-const state = {
-  selectedFont: '',
-  fontUrl: {
-    baseUrl: 'https://fonts.googleapis.com/css2?family=',
-    weight: ':wght@400;700;900',
+interface Font {
+  baseUrl: string
+  queryParams: {
+    display: string
+    family: string
+    text: string
+    weight: string
+  }
+}
+
+interface State {
+  fontURL: string
+  selectedFont: string
+  lastSelectedFontEl: HTMLElement | null
+  selectedCharacterSet: string
+  characterSet: string
+  showVariableFontsOnly: boolean
+  specialCharacters: string
+  queryString: string
+}
+
+const font: Font = {
+  baseUrl: 'https://fonts.googleapis.com/css2',
+  queryParams: {
     display: '&display=swap',
+    family: '?family=',
+    text: '&text=',
+    weight: ':wght@400;700;900',
   },
-  lastFontEl: '',
+}
+
+const state: State = {
+  fontURL: '',
+  selectedFont: 'Inter',
+  lastSelectedFontEl: null,
+  selectedCharacterSet: '',
+  characterSet: 'abcd',
+  showVariableFontsOnly: false,
+  specialCharacters: '',
+  queryString: '',
 }
 
 const headEl = document.head
-const fontsContainerEl = document.querySelector('[data-fonts]')
-const previewTextEl = document.querySelector('[data-preview-text]')
-const characterTableEl = document.querySelector('[data-character-table')
+const fontsContainerEl = document.querySelector(
+  '[data-fonts]'
+) as HTMLDivElement
+const previewTextEl = document.querySelector(
+  '[data-preview-text]'
+) as HTMLParagraphElement
+const characterTableEl = document.querySelector(
+  '[data-character-table'
+) as HTMLDivElement
 
 for (const character of characterTable['English']) {
   const divEl = document.createElement('div')
   divEl.innerText = character
   divEl.className =
-    'bg-blue-900 rounded text-xl h-8 p-8 flex justify-center items-center'
+    'bg-indigo-900 rounded text-xl h-8 p-8 flex justify-center items-center'
   characterTableEl?.append(divEl)
 }
 
 const selectEl = document.createElement('select')
+selectEl.className = 'w-1/2 px-6 py-4 text-2xl border-none rounded-full'
 selectEl.name = 'fonts'
 selectEl.id = 'fonts'
 
@@ -40,18 +79,37 @@ for (const font of variableFonts) {
 fontsContainerEl?.append(selectEl)
 
 function loadFont(fontName: string) {
-  const { baseUrl, weight, display } = state.fontUrl
+  const {
+    baseUrl,
+    queryParams: { display, weight },
+  } = font
 
-  if (state.lastFontEl) {
-    headEl.removeChild(state.lastFontEl)
+  if (state.lastSelectedFontEl) {
+    headEl.removeChild(state.lastSelectedFontEl)
   }
 
   const linkEl = document.createElement('link')
   linkEl.href = `${baseUrl}${fontName}${weight}${display}`
   linkEl.rel = 'stylesheet'
 
-  state.lastFontEl = linkEl
+  state.lastSelectedFontEl = linkEl
   headEl?.append(linkEl)
+}
+
+async function getFontURL() {
+  const {
+    baseUrl,
+    queryParams: { text },
+  } = font
+  const { characterSet, selectedFont } = state
+
+  const queryString = `${baseUrl}${selectedFont}${text}${characterSet}`
+  const isUrl = /(http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/
+
+  const response = await (await fetch(queryString)).text()
+  const [fontURL] = response.match(isUrl) as [string]
+
+  state.queryString = fontURL
 }
 
 function updateSelectedFont(selectedFont: string) {
