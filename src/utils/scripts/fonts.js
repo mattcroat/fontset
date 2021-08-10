1c165d3697b39f73cf50507bbec61b14292f64fb
mattcroat <mattcroat.dev@gmail.com>
2021-08-10 20:59:52 +0200

    refactor: Fonts output

--- src/utils/scripts/fonts.js
+++ src/utils/scripts/fonts.js
@@ -1,52 +1,28 @@
-const fs = require('fs')
+const { readJSON, writeJSON } = require('./json')
 
-class Fonts {
-  constructor(filePath) {
-    this.filePath = filePath
-    this.googleFonts = this.#readJSON(this.filePath)
-  }
+function getVariableFonts(fonts) {
+  const variableFonts = fonts
+    .filter((font) => font.axes.length > 0)
+    .map((font) => font.family)
 
-  getVariableFonts() {
-    const variableFonts = this.googleFonts
-      .filter((font) => font.axes.length > 0)
-      .map((font) => font.family)
-
-    return variableFonts
-  }
-
-  parseFonts() {
-    const parsedFonts = this.googleFonts.map((font) => ({
-      family: font.family,
-      category: font.category,
-      weights: Object.keys(font.fonts),
-      axes: font.axes,
-    }))
-
-    return parsedFonts
-  }
-
-  #readJSON(filePath) {
-    try {
-      const fonts = fs.readFileSync(filePath, 'utf-8')
-      const { familyMetadataList } = JSON.parse(fonts)
-      return familyMetadataList
-    } catch (error) {
-      throw new Error(`💩 Something went wrong: ${error}`)
-    }
-  }
-
-  writeJSON(outputPath, data) {
-    try {
-      const fonts = JSON.stringify(data, null, 2)
-      fs.writeFileSync(outputPath, fonts, 'utf8')
-      console.log(`Success! 👍 Wrote to ${outputPath}.`)
-    } catch (error) {
-      throw new Error(`💩 Something went wrong: ${error}`)
-    }
-  }
+  return variableFonts
 }
 
-const fonts = new Fonts('data/fonts.json')
+function parseFonts(fonts) {
+  const parsedFonts = fonts.map((font) => ({
+    family: font.family,
+    category: font.category,
+    weights: Object.keys(font.fonts),
+    axes: font.axes,
+  }))
 
-const parsedFonts = fonts.parseFonts()
-fonts.writeJSON('data/parsedFonts.json', parsedFonts)
+  return parsedFonts
+}
+
+async function outputFonts() {
+  const { familyMetadataList } = readJSON('src/data/fonts.json')
+  const parsedFonts = parseFonts(familyMetadataList)
+  writeJSON('src/data/parsedFonts.json', parsedFonts)
+}
+
+outputFonts()
